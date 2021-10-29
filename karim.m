b498db0b667d5ccf79fcf52003dcd7968435d42a
Karim-124 <93186477+Karim-124@users.noreply.github.com>
2021-10-29 22:50:42 +0200

    Add files via upload

---
+++ karim.m
@@ -0,0 +1,34 @@
+
+%% number 2# 
+ a=[-7 5 -9;2 -1 2 ;1 -1 2] 
+b=[16 3 2 13;5 10 11 8;9 6 7 12;4 15 14 1]
+c=[4 2 -3;7 -7 9;3 -5 6]
+d=[6 3 2;2 12 -7;-1 6 2;-5 15 11]
+3*a
+5*c
+(3*a)-(5*c)
+2*b
+7*a
+(7*a)+(2*b)
+c*a
+transpose(d)
+c*transpose(d)
+%% number 2#
+zeros(3)
+zeros(2,5)
+ones(5)
+ones(2,6)
+size(d)
+zeros(size(d))
+diag([1 2 3 4])
+%% numbers 3#
+[a,b]
+[a;b]
+%% numbber 4 #
+ x=zeros(7,8)
+ x=diag([5 5 5 5 5 5 5])
+
+x(:,8) = 5
+%% number 5#
+a(2,:)
+a(:,2)
